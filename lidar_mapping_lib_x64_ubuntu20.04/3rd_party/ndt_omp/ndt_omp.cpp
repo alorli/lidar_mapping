0387794f53d35161e042c25d21e11d6031b161c8
alorli <alorli@163.com>
2021-12-18 15:46:04 +0800

    add 4 zip files

--- lidar_mapping_lib_x64_ubuntu20.04/3rd_party/ndt_omp/ndt_omp.cpp
+++
@@ -1,5 +0,0 @@
-#include <ndt_omp/ndt_omp.h>
-#include <ndt_omp/ndt_omp_impl.hpp>
-
-template class pclomp::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ>;
-template class pclomp::NormalDistributionsTransform<pcl::PointXYZI, pcl::PointXYZI>;
